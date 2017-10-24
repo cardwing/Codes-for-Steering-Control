4d127fea3de7089bb3f0470affd8b5295da131d7
cardwing <Yuenan_Hou@163.com>
2017-10-24 14:12:00 +0800

    Update read_str_num_from_txt.py

--- read_str_num_from_txt.py
+++ read_str_num_from_txt.py
@@ -10,7 +10,7 @@ with open("valid-predictions-epoch14.txt", "r") as out:
         item_list = line.split(" ")
         len_str = len(item_list)
         for i in range(len_str):
-            if len(item_list[len_str - i - 1]) != 0:
+            if len(item_list[len_str - i - 1]) != 0: # len('') = 0
                 num_data = num_data + 1
             if num_data == 2:
                 image_mae.append(item_list[len_str - i - 1])
