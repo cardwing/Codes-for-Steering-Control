122fd718f22932070de62bbe455a9e5283622289
HOU Yuenan <Yuenan_Hou@163.com>
2018-07-28 14:51:34 +0800

    Clean the repo

--- generate_avi.py
+++
@@ -1,3 +0,0 @@
-import cvbase as cvb
-
-video = cvb.frames2video('/home/cardwing/Downloads/self-driving-car-master/steering-models/community-models/rambo/demo_final', '/home/cardwing/Downloads/self-driving-car-master/steering-models/community-models/rambo/demo_final_new.avi', fps = 20, filename_tmpl = '{:d}.png', start = 0,  end = 1998)
